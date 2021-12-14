06f085996259299a515fa9113e4ae926d8b665cc
joelbolwijnpinell <joel.bolwijn@gmail.com>
2021-12-14 18:50:38 +0100

    database conected

--- src/main.java
+++ src/main.java
@@ -1,27 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        Data data = new Data();
-        data.menu();
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1:
-                System.out.println(1);
-                break;
-            case 2:
-                System.out.println(1);
-                break;
-            case 3:
-                System.out.println(1);
-                break;
-            case 4:
-                System.out.println(1);
-                break;
-            default:
-                System.out.println(2);
-                break;
+class Main {
+    public static void main(String args[]){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/adbs","root","admin");
+            //here sonoo is the database name, root is the username and root is the password
+            Statement stmt=con.createStatement();
+
+            ResultSet rs=stmt.executeQuery("select * from emp");
+
+            while(rs.next())
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+
+            con.close();
+
+        } catch(Exception e) { 
+            System.out.println(e);
         }
     }
 }
